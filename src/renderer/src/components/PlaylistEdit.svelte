d39d6b01a382758d61b9d910b5e4e3f465ca63ee
ViperBite03 <layitamartin@gmail.com>
2024-04-30 01:10:35 +0200

    sdasd

---
+++ src/renderer/src/components/PlaylistEdit.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Input from './Input.svelte'
+  export let playlistName: string = ''
+</script>
+
+<style lang="scss">
+</style>
+
+<div class="playlist-edit">
+  <div class="photo"></div>
+  <Input placeholder="Playlist name" bind:value={playlistName} />
+</div>
